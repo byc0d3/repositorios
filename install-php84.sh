6ac3a5d399ff594eca1a58be83d7b25d57e44d0d
Johann Pineda <johann.pineda@gmail.com>
2024-10-04 19:04:29 -0400

    update

--- install-php84.sh
+++ install-php84.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-dnf module list php
-dnf config-manager --set-enabled powertools
+dnf -y update
+dnf -y install epel-release
 dnf -y --assumeyes install https://rpms.remirepo.net/enterprise/remi-release-9.rpm
 dnf -y --assumeyes module reset php
-dnf -y --assumeyes module install php:remi-8.3
+dnf -y --assumeyes module install php:remi-8.4
 dnf -y --assumeyes install php-intl php-mbstring php-cli php-xml php-mysqlnd php-gd php-curl php-zip php-json php-imagick php-simplexml
