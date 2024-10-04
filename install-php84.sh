9d26e110075546a528e0190f05ba2a4d2b7e0d2f
root <root@PC-HOME.>
2024-10-04 18:40:22 -0400

    update

--- install-php84.sh
+++ install-php84.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-dnf config-manager --set-enabled powertools
+dnf -y install epel-release
 dnf -y install https://rpms.remirepo.net/enterprise/remi-release-9.rpm
-dnf -y module reset php
+dnf -y update
+
+dnf config-manager --set-enabled remi-safe
+
+dnf -y module reset php -y
 dnf -y module install php:remi-8.4
 dnf -y install php-intl php-mbstring php-cli php-xml php-mysqlnd php-gd php-curl php-zip php-json php-imagick php-simplexml
