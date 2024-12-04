1bee15225803ecf2bfed26c72c4399d51cf2bcaa
byc0d3 <johann.pineda@gmail.com>
2024-12-04 12:18:48 -0400

    update

--- instalar-server-lapm.sh
+++ instalar-server-lapm.sh
@@ -1,37 +1,40 @@
 #!/bin/bash
 
-# Update all packages
+# Actualizar todos los paquetes
 dnf -y update
 
-# Install httpd
+# Instalar httpd
 dnf -y install httpd
 
-# Enable and start httpd
+# Habilitar y empezar httpd
 systemctl enable httpd --now
 
-# Install necessary packages and PHP modules
+# Instalar paquetes necesarios y módulos PHP
 dnf -y install epel-release
 dnf -y --assumeyes install https://rpms.remirepo.net/enterprise/remi-release-9.rpm
 dnf -y --assumeyes module reset php
 dnf -y --assumeyes module install php:remi-8.3
 dnf -y --assumeyes install php-intl php-mbstring php-cli php-xml php-mysqlnd php-gd php-curl php-zip php-json php-imagick php-simplexml
 
-# Restart httpd to apply changes
+# Reiniciar httpd para aplicar cambios
 systemctl restart httpd
 
-# Install Composer
+# Instalar Composer
 wget https://getcomposer.org/installer -O composer-installer.php
 php composer-installer.php --filename=composer --install-dir=/usr/local/bin
 
-# Restart httpd again
+# Reiniciar httpd nuevamente
 systemctl restart httpd
 
-# Setup MariaDB repository and install MariaDB
-curl -LsS https://downloads.mariadb.com/MariaDB/mariadb_repo_setup | bash
+# Configurar el repositorio de MariaDB e instalar MariaDB
+wget https://downloads.mariadb.com/MariaDB/mariadb_repo_setup | bash
 dnf -y --assumeyes install mariadb-server mariadb
 
-# Enable and start MariaDB
+# Habilitar y empezar MariaDB
 systemctl enable mariadb
 systemctl start mariadb
+
+rm -f composer-installer.php
+rm -f mariadb-server mariadb
 
 echo "Script completado. Todos los servicios han sido instalados y configurados."
