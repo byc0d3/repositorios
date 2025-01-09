76498217bc0ea2740afdfa24367313ed26ea9f8c
Johann Pineda <johann.pineda@gmail.com>
2025-01-09 07:34:13 -0400

    Update instalar-server-lapm.sh

--- instalar-server-lapm.sh
+++ instalar-server-lapm.sh
@@ -13,7 +13,7 @@ systemctl enable httpd --now
 dnf -y install epel-release
 dnf -y --assumeyes install https://rpms.remirepo.net/enterprise/remi-release-9.rpm
 dnf -y --assumeyes module reset php
-dnf -y --assumeyes module install php:remi-8.4
+dnf -y --assumeyes module install php:remi-8.3
 dnf -y --assumeyes install php-intl php-mbstring php-cli php-xml php-mysqlnd php-gd php-curl php-zip php-json php-imagick php-simplexml
 
 # Reiniciar httpd para aplicar cambios
